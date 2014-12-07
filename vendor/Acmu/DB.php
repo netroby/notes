90836f436371e84b362a6d149a10b13c85a6bec6
ZhiFeng Hu <hufeng1987@gmail.com>
2014-12-07 22:09:11 +0800

    PDO Error mode

--- vendor/Acmu/DB.php
+++ vendor/Acmu/DB.php
@@ -15,7 +15,8 @@ class DB
             $mysql_server = sprintf("mysql:host=%s;dbname=%s", $config['db']['host'], $config['db']['dbname']);
             $charset_info = sprintf("SET NAMES %s", $config['db']['charset']);
             static::$_instance = new PDO($mysql_server, $config['db']['user'], $config['db']['password'], array(
-                PDO::MYSQL_ATTR_INIT_COMMAND => $charset_info
+                PDO::MYSQL_ATTR_INIT_COMMAND => $charset_info,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             ));
         }
         return static::$_instance;

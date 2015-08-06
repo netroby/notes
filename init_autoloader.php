992b50c70a631d8fe9fc13c13a694660f61bcc73
ZhiFeng Hu <hufeng1987@gmail.com>
2015-08-06 08:43:59 +0800

    Fix bug in init autoloader

--- init_autoloader.php
+++ init_autoloader.php
@@ -1,6 +1,6 @@
 <?php
 set_error_handler(function ($severity, $message, $file, $line) {
     error_log(((string) new ErrorException($message, 0, $severity, $file, $line)) . PHP_EOL);
-})
+});
 include 'vendor/autoload.php';
 

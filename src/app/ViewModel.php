9afc4c15ae6eca7751925902da6d270a19485be8
ZhiFeng Hu <hufeng1987@gmail.com>
2015-09-30 08:21:35 +0800

    添加视图层
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

---
+++ src/app/ViewModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace notes\app;
+
+
+class ViewModel
+{
+    public function __construct($name)
+    {
+        echo getcwd();
+    }
+}
--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -1,18 +1,20 @@
 <?php
 namespace notes\controller;
 
+use notes\app\ViewModel;
+
 class HomeController
 {
     public function __construct()
     {
         session_start();
-        if (!isset($_SESSION['admin_user'])) {
+        if (!array_key_exists('admin_user', $_SESSION)) {
             header('location:/login');
         }
     }
     public function index()
     {
         echo 'home:index';
-        return false;
+        return new ViewModel('index');
     }
 }

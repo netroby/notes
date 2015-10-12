f2cbe4b574a7ea50f27f6be96a5ab78a8254fa13
ZhiFeng Hu <hufeng1987@gmail.com>
2015-10-13 07:32:48 +0800

    Update NoteController
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- src/controller/NoteController.php
+++ src/controller/NoteController.php
@@ -3,8 +3,37 @@ namespace notes\controller;
 
 class NoteController
 {
+    public function __construct()
+    {
+        session_start();
+        if (!array_key_exists('admin_user', $_SESSION)) {
+            header('location: login.php');
+        }
+    }
+
     public function index()
     {
         echo "hello";
+    }
+
+    public function delete()
+    {
+        if (array_key_exists('id', $_GET) && $_GET['id'] > 0) {
+            $id = (int) $_GET['id'];
+            include '../config/config.php';
+            include '../vendor/Acmu/DB.php';
+            $db = Acmu\Db::getInstance($config);
+            $mysql_server = null;
+            $charset_info = null;
+            $addtime = date('Y-m-d H:i:s', time());
+            $stmt = $db->prepare('update notes set is_delete = 1 where id = :id limit 1;');
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $note = $stmt->fetch(PDO::FETCH_ASSOC);
+            $dbh = null;
+            $addtime = null;
+
+        }
+        echo "Operation successed. <a href=\"javascript:top.location='list.php'\">OK</a>";
     }
 }

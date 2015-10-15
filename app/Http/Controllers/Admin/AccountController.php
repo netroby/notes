2bbcd6fb3042ff073502c8a89e775757f6ccbbe3
ZhiFeng Hu <hufeng1987@gmail.com>
2015-10-15 08:26:46 +0800

    Update files
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -9,30 +9,28 @@ class AccountController extends BaseController
 {
     public function login()
     {
-        if (isset($_SESSION['msg'])) {
-            $_SESSION['msg'] = NULL;
+        if (array_key_exists('msg', $_SESSION)) {
+            $_SESSION['msg'] = null;
             unset($_SESSION['msg']);
         }
         return view('admin.account.login');
     }
     public function doLogin()
     {
-        session_start();
-        include 'config/config.php';
         $gUsername = $gPassword = '';
-        if (!isset($_POST['username'])) {
+        if (!array_key_exists('username', $_POST)) {
             $_SESSION['msg'] = 'Username not provided';
             return header('location:/login');
         } else {
             $gUsername = htmlspecialchars(trim($_POST['username']));
         }
-        if (!isset($_POST['password'])) {
+        if (!array_key_exists('password', $_POST)) {
             $_SESSION['msg'] = 'Password not provided';
         } else {
             $gPassword = htmlspecialchars(trim($_POST['password']));
         }
 
-        if ($gUsername != $config['username'] || $gPassword != $config['password']) {
+        if ($gUsername !== $config['username'] || $gPassword !== $config['password']) {
             $_SESSION['msg'] = 'Login failed';
         } else {
             $_SESSION['admin_user'] = $config['username'];
